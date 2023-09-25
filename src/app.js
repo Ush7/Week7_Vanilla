e9f2593e5c45ab09c795a2c619fc15459e9be501
Ush7 <ush.shilp@gmail.com>
2023-09-25 15:10:33 -0400

    Search Engine Work JS

--- src/app.js
+++ src/app.js
@@ -1,9 +1,12 @@
 function formatDate(timestamp) {
   let date = new Date(timestamp);
   let currentHour = date.getHours();
+  if (currentHour < 10) {
+    currentHour = `0${currentHour}`;
+  }
   let currentMinutes = date.getMinutes();
   if (currentMinutes < 10) {
-    currentMinutes = "0" + currentMinutes;
+    currentMinutes = `0${currentMinutes}`;
   }
   let days = [
     "Sunday",
@@ -16,6 +19,12 @@ function formatDate(timestamp) {
   ];
   let currentDay = days[date.getDay()];
   return `${currentDay} ${currentHour}:${currentMinutes}`;
+}
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let currentDay = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  return days[currentDay];
 }
 
 function displayWeatherCondition(response) {
@@ -34,17 +43,26 @@ function displayWeatherCondition(response) {
     response.data.wind.speed
   );
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute("alt", response.data.weather[0].description);
 }
-
-function displayCityname(event) {
-  event.preventDefault();
+function search(city) {
   let apiKey = "8c48afa47a9a9c24f3500c7039d50aaa";
-  let city = document.querySelector("#enter-city-input").value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayWeatherCondition);
 }
+function displayCityname(event) {
+  event.preventDefault();
+  let city = document.querySelector("#enter-city-input");
+  search(city.value);
+}
+let enterCityform = document.querySelector("#enter-city-form");
+enterCityform.addEventListener("submit", displayCityname);
 
 let displayCity = document.querySelector("h2");
-let enterCityform = document.querySelector("#enter-city-form");
 let dateElement = document.querySelector("#date");
-enterCityform.addEventListener("submit", displayCityname);
+let iconElement = document.querySelector("#icon");
+search("Toronto");
